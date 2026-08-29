63a16c0df764287bdebb52381e0cbb946d439547
Mark-iwe2130 <bagdauletaltynbekov163@gmail.com>
2026-08-29 16:43:20 +0500

    Add files via upload

---
+++ example_mods/stages/pizeria.lua
@@ -0,0 +1,8 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('cara', 'cara', -50, -200);
+	setScrollFactor('cara', 1.0, 1.0);
+	scaleObject('cara', 1.6, 1.6);
+	
+	addLuaSprite('cara', false);
+end
